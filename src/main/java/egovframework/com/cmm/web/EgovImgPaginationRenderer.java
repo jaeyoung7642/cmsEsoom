38b4640b84ac57a710446834f7cb785f510e5b56
snb3205@naver.com <snb3205@naver.com>
2025-06-09 14:52:29 +0900

    example 파일 삭제

--- src/main/java/egovframework/com/cmm/web/EgovImgPaginationRenderer.java
+++ src/main/java/egovframework/com/cmm/web/EgovImgPaginationRenderer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package egovframework.example.cmmn.web;
+package egovframework.com.cmm.web;
 
 
 import java.text.MessageFormat;
